4985048c431264eb1b20e9e460c6c90739731236
Yisky-Game <e5wasd@hotmail.com>
2025-05-04 09:29:32 +0800

    Update vector_func.h

--- vector_func.h
+++ vector_func.h
@@ -1,31 +1,32 @@
+#ifndef _imported_vector_func
+#define _imported_vector_func
 #include <vector>
 #include <iostream>
 #include <cstring>
 #include <algorithm>
 #include <numeric>
-#include "mfdef.h"
-namespace much{
+#include <random>
+using std::vector;
+namespace much {
     // operator
-	#if __cplusplus >= 201103L
     template<typename T>
-    std::ostream& operator <<(std::ostream& outs, Vector<T> &a) {
+    std::ostream& operator <<(std::ostream& outs, vector<T> &a) {
         for (auto x : a)
             outs << x << ' ';
         return outs;
     }
-	#endif
 
     template<typename T>
-    std::istream& operator >>(std::istream& ins, Vector<T> &a) {
-        for (I_32 i = 0; i < a.size(); i++)
+    std::istream& operator >>(std::istream& ins, vector<T> &a) {
+        for (int i = 0; i < a.size(); i++)
             ins >> a[i];
         return ins;
     }
 	// iofunc.h
 	// 10_2
 	template<typename T>
-	void input_some(Vector<T> &a, const I_32 len) {
-		for (I_32 i = 0; i < len; i++) {
+	void input_some(vector<T> &a, const int len) {
+		for (int i = 0; i < len; i++) {
 			T sth = input(sth);
 			a.push_back(sth);
 		}
@@ -33,7 +34,7 @@ namespace much{
 
 	// 11_2
 	template<typename T>
-	void output_some(Vector<T> &a, const STR Begin = "", const STR end = " ", const STR Lend = "\n") {
+	void output_some(vector<T> &a, const std::string Begin = "", const std::string end = " ", const std::string Lend = "\n") {
 		for (T x : a)
 			output(x, Begin, end);
 		std::cout << Lend;
@@ -41,15 +42,15 @@ namespace much{
 
 	// 20_2
 	template<typename T>
-	void input_some_a_to_b(Vector<T> &a, const I_32 A, const I_32 B) {
-		for (I_32 i = A; i <= B; ++i)
+	void input_some_a_to_b(vector<T> &a, const int A, const int B) {
+		for (int i = A; i <= B; ++i)
 			input(a[i]);
 	}
 
 	// 21_2
 	template<typename T>
-	void output_some_a_to_b(Vector<T> &a, const I_32 A, const I_32 B, const STR Begin = "", const STR end = " ", const STR Lend = "\n") {
-		for (I_32 i = A; i <= B; ++i)
+	void output_some_a_to_b(vector<T> &a, const int A, const int B, const std::string Begin = "", const std::string end = " ", const std::string Lend = "\n") {
+		for (int i = A; i <= B; ++i)
 			output(a[i], Begin, end);
 		std::cout << Lend;
 	}
@@ -58,63 +59,96 @@ namespace much{
 	// much_func.h
 	// 5_2
 	template<typename T>
-	double avg(Vector<T> &nums) {
+	double avg(vector<T> &nums) {
 		T sum = std::accumulate(nums.begin(), nums.end(), T(0));
 		return static_cast<double>(sum) / nums.size();
 	}
 
 	// 9_2
 	template<typename T>
-	T sum_some(Vector<T> &a) {
+	T sum_some(vector<T> &a) {
 		return std::accumulate(a.begin(), a.end(), T(0));
 	}
 
 	// 12_2
 	template<typename T>
-	void n_to_some(Vector<T> &a, const T n) {
+	void n_to_some(vector<T> &a, const T n) {
 		std::fill_n(a.begin(), a.size(), n);
 	}
 
 	// 16_2
 	template<typename T>
-	T nums_max(Vector<T> &nums) {
+	T nums_max(vector<T> &nums) {
 		return *std::max_element(nums.begin(), nums.end());
 	}
 
 	// 17_2
 	template<typename T>
-	T nums_min(Vector<T> &nums) {
+	T nums_min(vector<T> &nums) {
 		return *std::min_element(nums.begin(), nums.end());
 	}
 
 	// 22_2
 	template<typename T>
-	void run_to_some(Vector<T> &a, const I_32 step = 1) {
-		for (I_32 i = 0; i < a.size(); i += step)
+	void run_to_some(vector<T> &a, const int step = 1) {
+		for (int i = 0; i < a.size(); i += step)
 			a[i] = i;
 	}
 
 	// 25_2
 	template<typename T>
-	void reverse_some(Vector<T> &a) {
+	void reverse_some(vector<T> &a) {
 		std::reverse(a.begin(), a.end());
 	}
 
 	// 27_2
 	// wrong.
-
-    // 33_2
-	//Please call srand(time(NULL))
+	
+	// 29_2
 	template <typename T>
-	void fill_random(Vector<T> &nums, I_32 len, I_64 max_rand) {
-		for (I_32 i = 0; i < len; i++)
-			nums[i] = rand() % max_rand;
+	vector<T> merge(vector<T> a, vector<T> b, vector<T>& obj) {
+		obj = a;
+		for (auto x : b)
+			obj.emplace_back(x);
+		return obj;
 	}
 
-	// 37
+	// 31_3
+	template <typename rT>
+	void fill_random(vector<int> *nums, rT rtype, const int len, const int maxr) {
+		for (int i = 0; i < len; i++)
+			nums[i] = rtype() % maxr;
+	}
+	
+	// 31_4
+	template <typename rT>
+	void fill_random(vector<long long> *nums, rT rtype, const int len, const long long maxr) {
+		for (int i = 0; i < len; i++)
+			nums[i] = rtype() % maxr;
+	}
+	
+    // 33_3
+	// Please call srand(time(NULL))
+	template <typename T>
+	void fill_random(vector<T> &nums, long long maxr) {
+		int len = nums.size();
+		for (int i = 0; i < len; i++)
+			nums[i] = rand() % maxr;
+	}
+	
+	// 33_4
+	// Please call srand(time(NULL))
+	template <typename T>
+	void fill_random(vector<T> &nums, int maxr) {
+		int len = nums.size();
+		for (int i = 0; i < len; i++)
+			nums[i] = rand() % maxr;
+	}
+	
+	// 41
 	template<typename T>
-	Vector<T> vector_range(T len, T _begin = 0, T step = 1) {
-		Vector<T> Range;
+	vector<T> vector_range(T len, T _begin = 0, T step = 1) {
+		vector<T> Range;
 		T num = _begin;
 		for (T i = 0; i < len; i++) {
 			Range.push_back(num);
@@ -122,4 +156,16 @@ namespace much{
 		}
 		return Range;
 	}
+	
+	// 42_2
+	template <typename T, typename _rdt, typename _sdt>
+	void fill_random_ab(vector<T>& nums, const T a, const T b, const _sdt sd) {
+		std::default_random_engine eng;
+		_rdt rd(a, b);
+		eng.seed(sd);
+		int len = nums.size();
+		for (int i = 0; i < len; i++)
+			nums[i] = rd(eng);
+	}
 }
+#endif
